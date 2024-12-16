87e4a3bb5cc86df62559ec9b95d18c341c4815a6
maverick-uni <reifgesma87304@th-nuernberg.de>
2024-12-16 22:18:19 +0100

    Update control.lua

--- mods/production_data_logger/control.lua
+++ mods/production_data_logger/control.lua
@@ -1,11 +1,11 @@
 -- control.lua
 
 script.on_event(defines.events.on_tick, function(event)
-    if event.tick % 300 == 0 then
+    if event.tick % 60 == 0 then
         local force = game.forces["player"] -- "player" ist die Standard-Fraktion
         local production_stats = force.get_item_production_statistics(1)
 
-local items = {"coal"}
+local items = {"coal", "copper-plate", "iron-plate"}
         
         for i = 1, #items do
             local item = items[i]
